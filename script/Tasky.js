95de85e989d1619b640bcef27b459a14f6ced910
irfan <kukkukt00716@gmail.com>
2023-01-31 14:38:06 +0530

    tasky application done

--- script/Tasky.js
+++ script/Tasky.js
@@ -59,7 +59,7 @@ const cardLayout = (card) => {
     card.id
   } class="card p-2 col-md-6 col-lg-4" style="width: 18rem">
   <div class="d-flex gap-2 justify-content-end mb-2">
-    <button onclick="editItem('${card.id}')" class="btn btn-outline-info">
+    <button onclick="editItem(event)" class="btn btn-outline-info">
       <i class="fa fa-pencil-alt"></i>
     </button>
     <button onclick="deleteItem('${card.id}')" class="btn btn-outline-danger">
@@ -98,7 +98,7 @@ const showBig = (title, img, description, date) => {
 
   const tem = `<div class="modal-content">
   <div class="modal-header">
-    <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
+    <h4 class="modal-title" id="exampleModalLabel">${title}</h4>
     <button
       type="button"
       class="btn-close"
@@ -108,16 +108,83 @@ const showBig = (title, img, description, date) => {
   </div>
   <div class="modal-body">
     <img src="${img}" alt="..." class="img-fluid"  />
-    <i>${date}</i>
-    <p>${description}</p>
+    <i>Created on ${date}</i>
+    <h6 class="text-muted">${description}</h6>
   </div>
 </div>`;
 
   cardBig.innerHTML = tem;
 };
 
-const editItem = (id) => {
+const editItem = (event) => {
   console.log("edit");
+  let tg = event.target;
+  let parent;
+
+  if (tg.tagName == "BUTTON") parent = tg.parentNode.parentNode;
+  else parent = tg.parentNode.parentNode.parentNode;
+
+  // console.log(parent);
+
+  let title = parent.childNodes[5].childNodes[1];
+  let description = parent.childNodes[5].childNodes[3];
+  let type = parent.childNodes[5].childNodes[5];
+  // console.log(title, description, type);
+  title.setAttribute("contenteditable", "true");
+  description.setAttribute("contenteditable", "true");
+  type.setAttribute("contenteditable", "true");
+
+  let submit = parent.childNodes[7];
+  submit.removeAttribute("data-bs-toggle");
+  submit.removeAttribute("data-bs-target");
+  submit.innerHTML = "Save Changes";
+  submit.setAttribute("onclick", "saveEdits(event)");
+};
+
+const saveEdits = (event) => {
+  let tg = event.target;
+  let parent = tg.parentNode;
+  let id = parent.id;
+  console.log(parent.id);
+
+  let title = parent.childNodes[5].childNodes[1];
+  let description = parent.childNodes[5].childNodes[3];
+  let type = parent.childNodes[5].childNodes[5];
+  let submit = parent.childNodes[7];
+
+  const eItem = {
+    id: id,
+    title: title.innerHTML,
+    description: description.innerHTML,
+    type: type.innerHTML,
+  };
+
+  submit.innerHTML = "Open Task";
+  let cp = state.taskList;
+  cp = cp.map((item) =>
+    item.id == eItem.id
+      ? {
+          id: eItem.id,
+          title: eItem.title,
+          description: eItem.description,
+          type: eItem.type,
+          img: item.img,
+          date: item.date,
+        }
+      : item
+  );
+  state.taskList = cp;
+  updateStorage();
+
+  title.setAttribute("contenteditable", "false");
+  description.setAttribute("contenteditable", "false");
+  type.setAttribute("contenteditable", "false");
+
+  submit.setAttribute("data-bs-toggle", "modal");
+  submit.setAttribute("data-bs-target", "#exampleModal1");
+  var tmp = state.taskList.find((item) => item.id == id);
+  var temp = `'${tmp.title}','${tmp.img}','${tmp.description}','${tmp.date}'`;
+  submit.setAttribute("onclick", `showBig(${temp})`);
 };
 
 const deleteItem = (id) => {
